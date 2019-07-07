9584288f93ba71ec5d20490211122fac28ff0dd6
mohanreddy070 <mohanreddy070@outlook.com>
2019-07-07 16:35:33 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from './http.service';
+import {product} from './product';
+import {Response, Request} from '@angular/http';
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular';
+@Component({selector: 'my-app', templateUrl: './app.component.html'})
+export class AppComponent implements OnInit {
+    constructor(private Http : HttpClient) {
+        this.URL ='http://localhost:3000/api';
+    }
+    cart : product[];
+    URL : string;
+    ngOnInit() {
+        this.Http.get(this.URL)
+            .map((response : Response) => response.json())
+            .subscribe((result:any) => {
+                this.cart = result;
+            });
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import {FormsModule} from '@angular/forms';
+import LoaderComponent from './loader/loader-component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import HeaderComponent from './header/app-header';
+import FooterComponent from './footer/app-footer';
+import {LoaderService} from './loader/loader-service';
+import {HttpClient} from './http.service';
+import shoppingCart from './cart/shopping-cart';
+//import {LocalStorageService} from './local.storage.service';
+import {HttpModule} from '@angular/http';
+import shoppingCartItem from './cart/shopping-cart-item';
+import shoppingList from './cart/shopping-list';
+import {cartService} from './cart/cartService';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule, FormsModule, HttpModule
+    ],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        LoaderComponent,
+        shoppingCart,
+        shoppingCartItem,
+        shoppingList
+    ],
+    providers: [
+        LoaderService, HttpClient, cartService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
