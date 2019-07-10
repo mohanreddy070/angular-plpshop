b9a03fb04b8d470160ee19439b560c82349b4e30
mohanreddy070 <mohanreddy070@outlook.com>
2019-07-10 11:19:59 +0530

    3 files modified

--- src/app/cart/shopping-cart.ts
+++ src/app/cart/shopping-cart.ts
@@ -14,7 +14,19 @@ export default class shoppingCart implements OnInit {
     ngOnInit(){
       // ocking as this ide doesn't support server side 
       //console.log(this.cart_Service.getAllProducts());
-      //this.Products =  productsCollection;
-      this.Products =  this.cart_Service.getAllProducts;
+      this.Products =  productsCollection;
+      //this.Products =  this.cart_Service.getAllProducts;
     }
+
+    descendingPriceData() {
+    return this.Products.sort((a, b) => {
+      return b.price - a.price;
+    });
+  }
+
+   ascendingPriceData() {
+    return this.Products.sort((a, b) => {
+      return a.price - b.price;
+    });
+   }
 }
--- src/app/product.ts
+++ src/app/product.ts
@@ -9,7 +9,8 @@ export const productsCollection = [
     price: 499.99,
     currency: 'EUR',
     image: 'images/01.jpg',
-    url: 'https://static2.jassets.com/p/Puma-917-Mid-2.0-Ind.-Blue-Sneakers-4118-935263-1-product2.jpg'
+    url: 'https://static2.jassets.com/p/Puma-917-Mid-2.0-Ind.-Blue-Sneakers-4118-935263-1-product2.jpg',
+    rating: 4
   },
   {
     id: 2,
@@ -17,7 +18,8 @@ export const productsCollection = [
     price: 249.99,
     currency: 'EUR',
     image: 'images/02.jpg',
-    url: 'http://scene7.zumiez.com/is/image/zumiez/pdp_hero/adidas-Flashback-White-%26-Black-Shoes-_272010-front-US.jpg'
+    url: 'http://scene7.zumiez.com/is/image/zumiez/pdp_hero/adidas-Flashback-White-%26-Black-Shoes-_272010-front-US.jpg',
+    rating: 4.5
   },
   {
     id: 4,
@@ -25,7 +27,8 @@ export const productsCollection = [
     price: 239.99,
     currency: 'EUR',
     image: 'images/04.jpg',
-    url: 'https://images-na.ssl-images-amazon.com/images/I/810h11HFM3L._UY395_.jpg'
+    url: 'https://images-na.ssl-images-amazon.com/images/I/810h11HFM3L._UY395_.jpg',
+    rating: 3.4
   },
   {
     id: 6,
@@ -33,7 +36,8 @@ export const productsCollection = [
     price: 119.99,
     currency: 'EUR',
     image: 'images/06.jpg',
-    url: 'http://assets.myntassets.com/assets/images/1920391/2017/6/8/11496905404701-Adidas-Men-Sports-Shoes-3121496905404588-5.jpg'
+    url: 'http://assets.myntassets.com/assets/images/1920391/2017/6/8/11496905404701-Adidas-Men-Sports-Shoes-3121496905404588-5.jpg',
+    rating: 4.1
   },
   {
     id: 5,
@@ -41,7 +45,8 @@ export const productsCollection = [
     price: 599.99,
     currency: 'EUR',
     image: 'images/05.jpg',
-    url: 'https://images-na.ssl-images-amazon.com/images/I/81t38mrch6L._UL1500_.jpg'
+    url: 'https://images-na.ssl-images-amazon.com/images/I/81t38mrch6L._UL1500_.jpg',
+    rating: 2.9
   },
   {
     id: 3,
@@ -49,7 +54,8 @@ export const productsCollection = [
     price: 149.99,
     currency: 'EUR',
     image: 'images/03.jpg',
-    url: 'https://cdn.shopclues.com/images/thumbnails/37609/320/320/INDUSNBLREDb14634988211480324580.jpg'
+    url: 'https://cdn.shopclues.com/images/thumbnails/37609/320/320/INDUSNBLREDb14634988211480324580.jpg',
+    rating: 3.5
   },
   {
     id: 9,
@@ -57,6 +63,7 @@ export const productsCollection = [
     price: 499.99,
     currency: 'EUR',
     image: 'images/01.jpg',
-    url: 'https://images-na.ssl-images-amazon.com/images/I/81t38mrch6L._UL1500_.jpg'
+    url: 'https://images-na.ssl-images-amazon.com/images/I/81t38mrch6L._UL1500_.jpg',
+    rating: 4.6
   }
 ];
